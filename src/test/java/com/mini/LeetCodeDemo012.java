00fbd6db85105ca396a53ac2149c11abf10a5f7b
邓粒 <dengli@zsyjr.com>
2022-05-06 18:11:07 +0800

    剑指 Offer II 012. 左右两边子数组的和相等

---
+++ src/test/java/com/mini/LeetCodeDemo012.java
@@ -0,0 +1,89 @@
+package com.mini;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+/**
+ *剑指 Offer II 012. 左右两边子数组的和相等
+ */
+public class LeetCodeDemo012 {
+
+    @Test
+    public void pivotIndexTest() {
+        int[] numbers1 = new int[]{1, 7, 3, 6, 5, 6};
+        int[] numbers2 = new int[]{1, 2, 3};
+        int[] numbers3 = new int[]{2, 1, -1};
+        System.out.println(JSON.toJSONString(pivotIndex(numbers1)));
+        System.out.println(JSON.toJSONString(pivotIndex(numbers2)));
+        System.out.println(JSON.toJSONString(pivotIndex(numbers3)));
+    }
+
+
+
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        // 譬如数组全部元素和为total，
+        //  譬如遍历到下标i，  那么 下标i之前的数之和 为sum， 下标i之后的数之和 为sum ，下标i的数值为 nums[i]
+        //  则total = sum + sum + nums[i]，则说明下标i就是 数组中心下标
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum * 2 + nums[i] == total) return i;
+            sum += nums[i];
+        }
+        return -1;
+    }
+
+    /**
+     *
+     给你一个整数数组nums ，请计算数组的 中心下标 。
+     数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+     如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+     如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+
+     示例 1：
+     输入：nums = [1,7,3,6,5,6]
+     输出：3
+     解释：
+     中心下标是 3 。
+     左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+     右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+
+     示例 2：
+     输入：nums = [1, 2, 3]
+     输出：-1
+     解释：
+     数组中不存在满足此条件的中心下标。
+
+     示例 3：
+     输入：nums = [2, 1, -1]
+     输出：0
+     解释：
+     中心下标是 0 。
+     左侧数之和 sum = 0 ，（下标 0 左侧不存在元素），
+     右侧数之和 sum = nums[1] + nums[2] = 1 + -1 = 0 。
+     */
+    public int pivotIndex1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = getSum(nums, i + 1, nums.length);
+            int leftSum = 0;
+            if (i != 0) {
+                leftSum = getSum(nums, 0, i);
+            }
+            if (rightSum == leftSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int getSum(int[] nums, int start, int end) {
+        int sum = 0;
+        for (int m = start; m < end; m++) {
+            sum += nums[m];
+        }
+        return sum;
+    }
+}
